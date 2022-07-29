88fc6f62977272cc8600dcbe9f4049de1a07b7f4
Furkan Çetinalp <cetinalp.furkan0@gmail.com>
2022-07-29 20:17:34 +0300

    Correction

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DTO.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController:ControllerBase
+    {
+        private readonly IAccountService _service;
+        public AccountController(IAccountService service)
+        {
+            _service = service;
+        }
+        //Printing all accounts
+        [HttpGet]
+        public IActionResult GetAllAccounts()
+        {
+            var data = _service.GetAll();
+            return Ok(data);
+        }
+        //Search an account by its 'AccountId'
+        [HttpGet("{id}")]
+        public IActionResult GetAccountById(int id)
+        {
+            var response = _service.GetByAccountId(id);
+            return Ok(response);
+        }
+        //Create a new account
+        [HttpPost]
+        public IActionResult Post(CreateAccountRequest request)
+        {
+            var response = _service.Insert(request);
+            return Ok(response);
+        }
+
+        //Change customerId for account if exists
+        [HttpPut("{Id}")]
+        public IActionResult Update(int Id, UpdateAccountRequest request)
+        {
+            var response = _service.Update(Id, request);
+            return Ok(response);
+        }
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            var response = _service.Delete(Id);
+            return Ok(response);
+        }
+
+    }
+}
---
+++ API/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using DTO.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController:ControllerBase
+    {
+        private readonly ICustomerService _service;
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
+
+        //Get all customers and their information
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            var data = _service.GetAll();
+            return Ok(data);
+        }
+        //Search customer by its identity number
+        [HttpGet("{identityNumber}")]
+        public IActionResult GetCustomerByIdentity(string identityNumber)
+        {
+            var response = _service.GetByIdentity(identityNumber);
+            return Ok(response);
+        }
+        [HttpPost]
+        public IActionResult Post(CreateCustomerRequest request)
+        {
+            var response = _service.Insert(request);
+            return Ok(response);
+        }
+
+        //Change customer info via identity number
+        [HttpPut("{identityNumber}")]
+        public IActionResult Update(string identityNumber, UpdateCustomerRequest request)
+        {
+            var response = _service.Update(identityNumber, request);
+            return Ok(response);
+        }
+        //Delete customer if exists
+        [HttpDelete("{identityNumber}")]
+        public IActionResult Delete(string identityNumber)
+        {
+            var response = _service.Delete(identityNumber);
+            return Ok(response);
+        }
+
+    }
+}
